f4a8417ef54a0a7c092481dbf58c55cd7d596a84
Connor Duffin <connor.p.duffin@gmail.com>
2022-09-13 10:01:24 +0100

    Tweaking of plotting script

--- scripts/plot_channel.py
+++ scripts/plot_channel.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 
 from argparse import ArgumentParser
 from swe import ShallowTwo
-from utils import dolfin_to_csr
 
 
 def plot_fields_curr(swe, t, output_dir):
@@ -54,31 +53,46 @@ def plot_quiver_curr(swe, t):
 
 
 parser = ArgumentParser()
-parser.add_argument("--integrate_continuity_by_parts", action="store_true")
-parser.add_argument("mesh_file", type=str)
 parser.add_argument("model_output_file", type=str)
 parser.add_argument("figure_output_dir", type=str)
 args = parser.parse_args()
 
 output = h5py.File(args.model_output_file, "r")
-t_final = output["t"][-1]
 
-swe = ShallowTwo(mesh=args.mesh_file,
+mesh_file = "mesh/channel-piggott.xdmf"
+swe = ShallowTwo(mesh=mesh_file,
                  control={
-                     "dt":
-                     1e-3,
-                     "theta":
-                     1,
-                     "simulation":
-                     "laminar",
-                     "integrate_continuity_by_parts":
-                     args.integrate_continuity_by_parts
+                     "dt": 1e-3,
+                     "theta": 1,
+                     "integrate_continuity_by_parts": True,
+                     "simulation": "cylinder",
+                     "laplacian": False,
+                     "les": True
                  })
 
-swe.set_curr_vector(output["du"][-1, :])
-swe.set_prev_vector(output["du"][-2, :])
+x_vertices = swe.mesh.coordinates()
+x, y = x_vertices[:, 0], x_vertices[:, 1]
+n_vertices = len(x_vertices)
 
-plot_quiver_curr(swe, t_final)
+vec_name = "/du/vector_100"
+checkpoint = fe.HDF5File(swe.mesh.mpi_comm(), args.model_output_file, "r")
+checkpoint.read(swe.du, vec_name)  # read into du
+t = checkpoint.attributes(vec_name)["timestamp"]
+
+# def extract_fields(swe, n_vertices):
+#     du_vec = swe.du.compute_vertex_values()
+#     u1, u2, h = (du_vec[:n_vertices], du_vec[n_vertices:(2 * n_vertices)],
+#                  du_vec[(2 * n_vertices):])
+#     return (u1, u2, h)
+
+# u1_curr, u2_curr, h_curr = extract_fields(swe, n_vertices)
+
+# t = checkpoint.attributes(vec_name)["timestamp"]
+# u1[i, :] = u1_curr
+# u2[i, :] = u2_curr
+# h[i, :] = h_curr
+
+plot_quiver_curr(swe, t)
 plt.savefig(args.figure_output_dir + "quiver.png", dpi=600)
 plt.close()
 
@@ -88,6 +102,6 @@ plt.savefig(args.figure_output_dir + "mesh.png", dpi=600)
 plt.close()
 
 # plot the solution field
-plot_fields_curr(swe, t_final, args.figure_output_dir)
+plot_fields_curr(swe, t, args.figure_output_dir)
 plt.savefig(args.figure_output_dir + "solution-fields.png", dpi=600)
 plt.close()
