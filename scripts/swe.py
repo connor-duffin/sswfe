74f481e1df9a0bafd51796a750b0123dddf53c8f
Connor Duffin <connor.p.duffin@gmail.com>
2022-09-13 09:59:29 +0100

    Added in BDF2 timestepper in lieu of IMEX

--- scripts/swe.py
+++ scripts/swe.py
@@ -223,7 +223,7 @@ class ShallowTwo:
         self.bcs = None
         self.solver = None
 
-    def setup_form(self, du, du_prev, du_prev_prev=None, imex=False):
+    def setup_form(self, du, du_prev, du_prev_prev=None, bdf2=False):
         g = fe.Constant(9.8)
         nu = fe.Constant(self.nu)
         C = fe.Constant(self.C)
@@ -242,31 +242,30 @@ class ShallowTwo:
         self.f_u = fe.Function(self.U_space)
         self.f_h = fe.Function(self.H_space)
 
-        if imex:
-            u_mid = self.theta * u + (1 - self.theta) * u_prev_prev
-            h_mid = self.theta * h + (1 - self.theta) * h_prev_prev
-            F = (fe.inner(u - u_prev_prev, v_u) / (2 * dt) * fe.dx  # mass term u
-                 + fe.inner(h - h_prev_prev, v_h) / (2 * dt) * fe.dx  # mass term h
-                 + fe.inner(fe.dot(u_prev, fe.nabla_grad(u_prev)), v_u) * fe.dx  # advection
-                 + C * u_mag * fe.inner(u_prev, v_u) / (self.H + h_prev) * fe.dx  # friction term
-                 + g * fe.inner(fe.grad(h_mid), v_u) * fe.dx  # surface term
+        if bdf2:
+            u_mid = u
+            h_mid = h
+            F = (3/2 * fe.inner(u - 4/3 * u_prev + 1/3 * u_prev_prev, v_u) / dt * fe.dx
+                 + 3/2 * fe.inner(h - 4/3 * h_prev + 1/3 * h_prev_prev, v_h) / dt * fe.dx
+                 + fe.inner(fe.dot(u, fe.nabla_grad(u)), v_u) * fe.dx  # advection
+                 + C * u_mag * fe.inner(u, v_u) / (self.H + h_prev) * fe.dx  # friction term
+                 + g * fe.inner(fe.grad(h), v_u) * fe.dx  # surface term
                  - fe.inner(self.f_u, v_u) * fe.dx
                  - fe.inner(self.f_h, v_h) * fe.dx)
         else:
-            # only difference
             u_mid = self.theta * u + (1 - self.theta) * u_prev
             h_mid = self.theta * h + (1 - self.theta) * h_prev
             F = (fe.inner(u - u_prev, v_u) / dt * fe.dx  # mass term u
                  + fe.inner(h - h_prev, v_h) / dt * fe.dx  # mass term h
                  + fe.inner(fe.dot(u_mid, fe.nabla_grad(u_mid)), v_u) * fe.dx  # advection
-                 + C * u_mag * fe.inner(u_mid, v_u) / (self.H + h_mid) * fe.dx  # friction term
+                 + C * u_mag * fe.inner(u_mid, v_u) / (self.H + h_prev) * fe.dx  # friction term
                  + g * fe.inner(fe.grad(h_mid), v_u) * fe.dx  # surface term
                  - fe.inner(self.f_u, v_u) * fe.dx
                  - fe.inner(self.f_h, v_h) * fe.dx)
 
         # add in (parameterised) dissipation effects
         if self.use_les:
-            self.les = LES(mesh=self.mesh, fs=self.H_space, u=u_prev, density=1.0, smagorinsky_coefficient=0.164)
+            self.les = LES(mesh=self.mesh, fs=self.H_space, u=u_mid, density=1.0, smagorinsky_coefficient=0.164)
             nu_t = self.les.eddy_viscosity
         else:
             nu_t = 0.
@@ -275,12 +274,8 @@ class ShallowTwo:
         F += dissipation
 
         # add in continuity term
-        if imex:
-            F += continuity_term(self.H, h_prev, u_prev, v_h,
-                                 self.integrate_continuity_by_parts)
-        else:
-            F += continuity_term(self.H, h_mid, u_mid, v_h,
-                                 self.integrate_continuity_by_parts)
+        F += continuity_term(self.H, h_mid, u_mid, v_h,
+                             self.integrate_continuity_by_parts)
 
         J = fe.derivative(F, du)
         return F, J
@@ -356,31 +351,39 @@ class ShallowTwo:
         problem = fe.NonlinearVariationalProblem(F, du, bcs=bcs, J=J)
         solver = fe.NonlinearVariationalSolver(problem)
 
-        # solver options
+        # vanilla fenics solver options
         prm = solver.parameters
+        # prm['newton_solver']['absolute_tolerance'] = 1E-8
+        # prm['newton_solver']['relative_tolerance'] = 1E-6
+        # prm['newton_solver']['convergence_criterion'] = "incremental"
+        # prm['newton_solver']['maximum_iterations'] = 50
+        # prm['newton_solver']['relaxation_parameter'] = 0.5
+
+        # PETSc SNES config
         prm["nonlinear_solver"] = "snes"
         prm["snes_solver"]["line_search"] = "bt"
         prm["snes_solver"]["linear_solver"] = "gmres"
+        # prm["snes_solver"]["linear_solver"] = "mumps"
         prm["snes_solver"]["preconditioner"] = "jacobi"
         logger.info(f"using {prm['snes_solver']['linear_solver']} solver with {prm['snes_solver']['preconditioner']} PC")
 
         # solver convergence
-        prm["snes_solver"]["relative_tolerance"] = 1e-7
+        prm["snes_solver"]["relative_tolerance"] = 1e-5
+        prm["snes_solver"]['absolute_tolerance'] = 1e-5
         prm["snes_solver"]["maximum_iterations"] = 50
+        prm["snes_solver"]['error_on_nonconvergence'] = True
+        prm["snes_solver"]['krylov_solver']['nonzero_initial_guess'] = True
 
         # solver reporting
         prm["snes_solver"]['krylov_solver']['report'] = False
         prm["snes_solver"]['krylov_solver']['monitor_convergence'] = False
 
         # don't print outputs from the Newton solver
-        # prm["snes_solver"]["report"] = False
-
-        # JIC we want to tweak tolerances
-        # prm["snes_solver"]["absolute_tolerance"] = 1e-6
+        prm["snes_solver"]["report"] = True
 
         return solver
 
-    def solve(self, imex):
+    def solve(self, bdf2):
         # set the LES parameter
         if self.use_les:
             self.les.solve()
@@ -389,7 +392,7 @@ class ShallowTwo:
         self.solver.solve()
 
         # set previous timesteps appropriately
-        if imex:
+        if bdf2:
             fe.assign(self.du_prev_prev, self.du_prev)
 
         fe.assign(self.du_prev, self.du)
@@ -417,7 +420,7 @@ class ShallowTwo:
 
     def setup_checkpoint(self, checkpoint_file):
         """ Set up the checkpoint file, writing the appropriate things etc. """
-        print(f"storing outputs in {checkpoint_file}")
+        logger.info(f"storing outputs in {checkpoint_file}")
         self.checkpoint = fe.HDF5File(self.mesh.mpi_comm(), checkpoint_file, "w")
 
     def checkpoint_save(self, t):
