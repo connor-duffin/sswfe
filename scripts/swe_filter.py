5cbf342998be6535eb4c8f76e2ea32da709cda54
Connor Duffin <connor.p.duffin@gmail.com>
2022-10-12 15:04:22 +0100

    Basics of filtering stuff added in

--- scripts/swe_filter.py
+++ scripts/swe_filter.py
@@ -9,20 +9,74 @@ from scipy.linalg import cholesky, cho_factor, cho_solve, eigh
 from statfenics.covariance import sq_exp_covariance
 from statfenics.utils import dolfin_to_csr
 
-from swe import ShallowOne
+from swe import ShallowOne, ShallowOneLinear
 
 # initialise the logger
 logger = logging.getLogger(__name__)
 
 
+class ShallowOneKalman(ShallowOneLinear):
+    def __init__(self, control, params, stat_params, lr=False):
+        super().__init__(control=control, params=params)
+        u, v = fe.TrialFunction(self.U_space), fe.TestFunction(self.U_space)
+        M_u = fe.assemble(fe.inner(u, v) * fe.dx)
+        M_u_scipy = dolfin_to_csr(M_u)
+
+        u, v = fe.TrialFunction(self.H_space), fe.TestFunction(self.H_space)
+        M_h = fe.assemble(fe.inner(u, v) * fe.dx)
+        M_h_scipy = dolfin_to_csr(M_h)
+
+        self.lr = lr
+        self.mean = self.du.vector().get_local()
+
+        # TODO: extend to allow forcing on multiple components
+        # assume that forcing is 'h'-only, for now
+        # rho = stat_params["rho"]
+        # ell = stat_params["ell"]
+        K_u = sq_exp_covariance(self.x_dofs_u,
+                                stat_params["u_cov"]["rho"],
+                                stat_params["u_cov"]["ell"])
+        K_h = sq_exp_covariance(self.x_dofs_h,
+                                stat_params["h_cov"]["rho"],
+                                stat_params["h_cov"]["ell"])
+
+        self.G = np.zeros((self.mean.shape[0], self.mean.shape[0]))
+        self.G[np.ix_(self.u_dofs, self.u_dofs)] = M_u_scipy @ K_u @ M_u_scipy.T
+        self.G[np.ix_(self.h_dofs, self.h_dofs)] = M_h_scipy @ K_h @ M_h_scipy.T
+        self.G[np.diag_indices_from(self.G)] += 1e-10
+
+        if self.lr:
+            self.k_init = stat_params["k_init"]
+            self.k = stat_params["k"]
+
+            # initialisation from above
+            self.G_vals, self.G_vecs = eigs(self.G, self.k_init)
+            self.G_vals, self.G_vecs = np.real(self.G_vals), np.real(self.G_vecs)
+            self.G_sqrt = self.G_vecs @ np.diag(np.sqrt(self.G_vals))
+
+            # covariance square-root
+            self.cov_sqrt = np.zeros((self.mean.shape[0], self.k))
+            self.cov_sqrt_prev = np.zeros((self.mean.shape[0], self.k))
+            self.cov_sqrt_pred = np.zeros((self.mean.shape[0], self.k + self.k_init))
+        else:
+            # normal covariance structure
+            self.cov = np.zeros((self.mean.shape[0], self.mean.shape[0]))
+            self.cov_prev = np.zeros((self.mean.shape[0], self.mean.shape[0]))
+            self.cov_pred = np.zeros((self.mean.shape[0], self.mean.shape[0]))
+
+        self.A_mat = fe.assemble(self.a)
+        self.A_prev = fe.derivative(self.L, self.du_prev)
+        self.A_prev_mat = fe.assemble(self.A_prev)
+
+        for A in [self.A_mat, self.A_prev_mat]: self.bcs.apply(A)
+
+        self.A_scipy = dolfin_to_csr(self.A_mat)
+        self.A_prev_scipy = dolfin_to_csr(self.A_prev_mat)
+
+
 class ShallowOneEx(ShallowOne):
     def __init__(self, control, params, stat_params, lr=False):
         super().__init__(control=control, params=params)
-
-        self.U, self.H = self.W.split()
-        self.U_space = self.U.collapse()
-        self.H_space = self.H.collapse()
-
         u, v = fe.TrialFunction(self.U_space), fe.TestFunction(self.U_space)
         M_u = fe.assemble(fe.inner(u, v) * fe.dx)
         M_u_scipy = dolfin_to_csr(M_u)
--- scripts/test_swe_filter.py
+++ scripts/test_swe_filter.py
@@ -9,10 +9,45 @@ from scipy.sparse import csr_matrix
 from statfenics.covariance import sq_exp_covariance
 from statfenics.utils import dolfin_to_csr
 
-from swe_filter import ShallowOneEx
+from swe_filter import ShallowOneEx, ShallowOneKalman
 
 
-def test_1d_filter():
+def test_1d_linear_filter():
+    control = {"nx": 32, "dt": 0.5, "theta": 1.0, "simulation": "immersed_bump"}
+    params = {"nu": 0.}
+    stat_params = {"h_cov": {"rho": 1e-2, "ell": 5000.},
+                   "u_cov": {"rho": 0., "ell": 5000.}}
+    swe = ShallowOneKalman(control, params, stat_params, lr=False)
+
+    # verify our construction/filtering is OK
+    du = fe.Function(swe.W)
+    du_prev = fe.Function(swe.W)
+    v_u, v_h = fe.TestFunctions(swe.W)
+
+    # fe.assign(du_prev.sub(1),
+    #           fe.interpolate(
+    #               fe.Expression("exp(- pow(2 * (x[0] - 10), 2)) / 80",
+    #                             degree=2), swe.H_space))
+
+    u, h = fe.split(du)
+    u_prev, h_prev = fe.split(du_prev)
+
+    dt = swe.dt
+    u_theta = swe.theta * u + (1 - swe.theta) * u_prev
+    h_theta = swe.theta * h + (1 - swe.theta) * h_prev
+
+    F = (fe.inner(h - h_prev, v_h) / dt * fe.dx
+         + (swe.H * u_theta).dx(0) * v_h * fe.dx
+         + fe.inner(u - u_prev, v_u) / dt * fe.dx
+         + 9.8 * h_theta.dx(0) * v_u * fe.dx)
+
+    J = fe.assemble(fe.derivative(F, du))
+    swe.bcs.apply(J)
+    J_scipy = dolfin_to_csr(J)
+    np.testing.assert_allclose(swe.A_scipy.todense(), J_scipy.todense())
+
+
+def test_1d_nonlinear_filter():
     control = {"nx": 32, "dt": 1., "theta": 1.0, "simulation": "tidal_flow"}
     params = {"nu": 1.}
     stat_params = {"h_cov": {"rho": 1e-2, "ell": 5000.},
