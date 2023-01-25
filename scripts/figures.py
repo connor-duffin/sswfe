d19acee56c994faaadd24c9be3c501d0452bf43d
Connor Duffin <connor.p.duffin@gmail.com>
2023-01-25 10:39:35 +0000

    Notebooks and settings for paper plots

---
+++ scripts/figures.py
@@ -0,0 +1,10 @@
+import palettable
+
+figure_sizes = {
+    "halfwidth": 3.54,
+    "fullwidth": 7.2
+}
+
+color_diverge = palettable.cartocolors.diverging.Geyser_3.mpl_colormap
+color_cycle = palettable.cartocolors.qualitative.Prism_6.mpl_colors
+color_map = palettable.matplotlib.Viridis_6.mpl_colormap
