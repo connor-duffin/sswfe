74e0a5b0f5096a056342d61bd2d895de72b63554
Connor Duffin <connor.p.duffin@gmail.com>
2023-06-16 10:21:09 +0100

    Fixed up solvers and H5 outputting

--- scripts/ns_2d_fenicsx.py
+++ scripts/ns_2d_fenicsx.py
@@ -1,3 +1,4 @@
+import h5py
 import gmsh
 import os
 import numpy as np
@@ -22,23 +23,9 @@ from ufl import (FacetNormal, FiniteElement, Identity, Measure,
                  TestFunction, TrialFunction, VectorElement, as_vector, div,
                  dot, ds, dx, inner, lhs, grad, nabla_grad, rhs, sym)
 
-gmsh.initialize()
 
-L = 5.46
-H = 1.85
-c_x = L / 2
-c_y = H / 2
-r = 0.05
 gdim = 2
-mesh_comm = MPI.COMM_WORLD
-model_rank = 0
 
-mesh, _, ft = gmshio.read_from_msh(
-    "mesh/branson-refined.msh", mesh_comm, model_rank, gdim=2)
-ft.name = "Facet markers"
-
-
-# Define boundary conditions
 class InletVelocity():
     def __init__(self, t):
         self.t = t
@@ -50,47 +37,68 @@ class InletVelocity():
 
 
 # Navier-Stokes solver
-# HACK(connor): basically a huge closure over all the mesh
 class NSSplit:
-    def __init__(self, dt, params):
-        self.dt = Constant(mesh, PETSc.ScalarType(dt))
-        self.mu = Constant(mesh, PETSc.ScalarType(params["mu"]))
-        self.rho = Constant(mesh, PETSc.ScalarType(params["rho"]))
+    def __init__(self, mesh_file, dt, params):
+        # L = 5.46
+        # H = 1.85
+        # c_x = L / 2
+        # c_y = H / 2
+        # r = 0.05
+        mesh_comm = MPI.COMM_WORLD
+        model_rank = 0
+
+        self.msh, _, self.ft = gmshio.read_from_msh(
+            mesh_file, mesh_comm, model_rank, gdim=gdim)
+        self.ft.name = "Facet markers"
+
+        self.dt = Constant(self.msh, PETSc.ScalarType(dt))
+        self.mu = Constant(self.msh, PETSc.ScalarType(params["mu"]))
+        self.rho = Constant(self.msh, PETSc.ScalarType(params["rho"]))
 
         # setup function spaces
-        v_cg2 = VectorElement("CG", mesh.ufl_cell(), 2)
-        s_cg1 = FiniteElement("CG", mesh.ufl_cell(), 1)
-        self.V = FunctionSpace(mesh, v_cg2)
-        self.Q = FunctionSpace(mesh, s_cg1)
-        self.fdim = mesh.topology.dim - 1
+        v_cg2 = VectorElement("CG", self.msh.ufl_cell(), 2)
+        s_cg1 = FiniteElement("CG", self.msh.ufl_cell(), 1)
+
+        self.V = FunctionSpace(self.msh, v_cg2)
+        self.V_dof_coordinates = self.V.tabulate_dof_coordinates()
+
+        self.Q = FunctionSpace(self.msh, s_cg1)
+        self.Q_dof_coordinates = self.Q.tabulate_dof_coordinates()
+        self.fdim = self.msh.topology.dim - 1
 
     def setup_form(self):
-        # mark relevant parts of the domain
+        # HACK(connor): hardcode and mark relevant parts of the domain
         inlet_marker, outlet_marker, wall_marker, obstacle_marker = 2, 3, 4, 5
 
-        # Inlet
-        self.u_inlet = Function(self.V)
-        self.inlet_velocity = InletVelocity(t)
-        self.u_inlet.interpolate(self.inlet_velocity)
-        bcu_inflow = dirichletbc(self.u_inlet,
-                                 locate_dofs_topological(
-                                     self.V, self.fdim, ft.find(inlet_marker)))
-
         # Walls
-        u_nonslip = np.array((0,) * mesh.geometry.dim, dtype=PETSc.ScalarType)
-        bcu_walls = dirichletbc(u_nonslip,
-                                locate_dofs_topological(
-                                    self.V, self.fdim, ft.find(wall_marker)), self.V)
+        u_nonslip = np.array((0, 0), dtype=PETSc.ScalarType)
+        bcu_walls = dirichletbc(
+            u_nonslip,
+            locate_dofs_topological(self.V, self.fdim, self.ft.find(wall_marker)),
+            self.V)
 
         # Obstacle
-        bcu_obstacle = dirichletbc(u_nonslip,
-                                   locate_dofs_topological(
-                                       self.V, self.fdim, ft.find(obstacle_marker)), self.V)
+        bcu_obstacle = dirichletbc(
+            u_nonslip,
+            locate_dofs_topological(self.V, self.fdim, self.ft.find(obstacle_marker)),
+            self.V)
+
+        # Inlet
+        # self.u_inlet = Function(self.V)
+        # self.inlet_velocity = InletVelocity(t)
+        # self.u_inlet.interpolate(self.inlet_velocity)
+        u_inlet = np.array((0.01, 0), dtype=PETSc.ScalarType)
+        bcu_inflow = dirichletbc(
+            u_inlet,
+            locate_dofs_topological(self.V, self.fdim, self.ft.find(inlet_marker)),
+            self.V)
         self.bcu = [bcu_inflow, bcu_obstacle, bcu_walls]
 
         # Outlet
-        bcp_outlet = dirichletbc(PETSc.ScalarType(0.),
-                                 locate_dofs_topological(self.Q, self.fdim, ft.find(outlet_marker)), self.Q)
+        bcp_outlet = dirichletbc(
+            PETSc.ScalarType(0.),
+            locate_dofs_topological(self.Q, self.fdim, self.ft.find(outlet_marker)),
+            self.Q)
         self.bcp = [bcp_outlet]
 
         # trial/test functions used for form creation, only.
@@ -109,7 +117,7 @@ class NSSplit:
         self.p_.name = "p"
         self.phi = phi = Function(self.Q)
 
-        f = Constant(mesh, PETSc.ScalarType((0, 0)))
+        f = Constant(self.msh, PETSc.ScalarType((0, 0)))
         F1 = self.rho / self.dt * dot(u - u_n, v) * dx
         F1 += inner(dot(1.5 * u_n - 0.5 * u_n1,
                         0.5 * nabla_grad(u + u_n)), v) * dx
@@ -136,14 +144,14 @@ class NSSplit:
         self.b3 = create_vector(self.L3)
 
         # solver for step 1
-        self.solver1 = PETSc.KSP().create(mesh.comm)
+        self.solver1 = PETSc.KSP().create(self.msh.comm)
         self.solver1.setOperators(self.A1)
         self.solver1.setType(PETSc.KSP.Type.BCGS)
         pc1 = self.solver1.getPC()
         pc1.setType(PETSc.PC.Type.JACOBI)
 
         # solver for step 2
-        self.solver2 = PETSc.KSP().create(mesh.comm)
+        self.solver2 = PETSc.KSP().create(self.msh.comm)
         self.solver2.setOperators(self.A2)
         self.solver2.setType(PETSc.KSP.Type.MINRES)
         pc2 = self.solver2.getPC()
@@ -151,15 +159,15 @@ class NSSplit:
         pc2.setHYPREType("boomeramg")
 
         # solver for step 3
-        self.solver3 = PETSc.KSP().create(mesh.comm)
+        self.solver3 = PETSc.KSP().create(self.msh.comm)
         self.solver3.setOperators(self.A3)
         self.solver3.setType(PETSc.KSP.Type.CG)
         pc3 = self.solver3.getPC()
         pc3.setType(PETSc.PC.Type.SOR)
 
     def solve(self, t):
-        self.inlet_velocity.t = t
-        self.u_inlet.interpolate(self.inlet_velocity)
+        # self.inlet_velocity.t = t
+        # self.u_inlet.interpolate(self.inlet_velocity)
 
         # step 1: tentative velocity step
         self.A1.zeroEntries()
@@ -199,36 +207,59 @@ class NSSplit:
         self.u_.x.scatter_forward()
 
         # update variable with solution form this time step
-        with self.u_.vector.localForm() as loc_, self.u_n.vector.localForm() as loc_n, self.u_n1.vector.localForm() as loc_n1:
+        with (self.u_.vector.localForm() as loc_,
+              self.u_n.vector.localForm() as loc_n,
+              self.u_n1.vector.localForm() as loc_n1):
             loc_n.copy(loc_n1)
             loc_.copy(loc_n)
 
 
-t = 0
-T = 120
-dt = 1 / 2000
-num_steps = int(T / dt)
+if __name__ == "__main__":
+    t = 0
+    T = 60.
+    dt = 1e-3
+    nt = int(T / dt)
+    thin = 100
+    nt_save = nt // thin
 
-params = dict(mu=1e-2, rho=1000)
-ns = NSSplit(dt, params)
-ns.setup_form()
+    if nt % thin >= 1:
+        nt_save += 1
 
-u_viewer = PETSc.Viewer().createMPIIO(
-    "outputs/branson-testing-u.dat", "w", mesh_comm)
-p_viewer = PETSc.Viewer().createMPIIO(
-    "outputs/branson-testing-p.dat", "w", mesh_comm)
+    params = dict(mu=1e-3, rho=1000)
+    mesh_file = "mesh/branson-refined.msh"
+    ns = NSSplit(mesh_file, dt, params)
+    ns.setup_form()
 
-progress = tqdm.autonotebook.tqdm(desc="Solving PDE", total=num_steps)
-for i in range(num_steps):
-    progress.update(1)
+    output_file = "outputs/branson-testing.h5"
+    output = h5py.File(output_file, "w")
 
-    t += dt
-    ns.solve(t)
+    metadata = params
+    for name, val in metadata.items():
+        output.attrs.create(name, val)
 
-    # check for divergences
-    if ((not np.any(np.isnan(ns.u_.vector.array))) and (not np.any(np.isnan(ns.p_.vector.array)))):
-        ns.u_.vector.view(u_viewer)
-        ns.p_.vector.view(p_viewer)
-    else:
-        print(f"simulation failed at t = {t:.4e}")
-        break
+    n_dofs_u = ns.V_dof_coordinates.shape[0]
+    n_dofs_p = ns.Q_dof_coordinates.shape[0]
+
+    t_out = output.create_dataset("t", shape=(nt_save, ), dtype=np.float64)
+    u_out = output.create_dataset("u_mean", shape=(nt_save, 2 * n_dofs_u), dtype=np.float64)
+    p_out = output.create_dataset("p_mean", shape=(nt_save, n_dofs_p), dtype=np.float64)
+
+    i_save = 0
+    progress = tqdm.autonotebook.tqdm(desc="Solving PDE", total=nt)
+    for i in range(nt):
+        progress.update(1)
+
+        t += dt
+        ns.solve(t)
+
+        # check for NaNs
+        if (np.any(np.isnan(ns.u_.vector.array)) and np.any(np.isnan(ns.p_.vector.array))):
+            print(f"Simulation failed at t = {t:.4e}")
+            break
+
+        # store outputs
+        if i % thin == 0:
+            t_out[i_save] = t
+            u_out[i_save, :] = ns.u_.vector.array.copy()
+            p_out[i_save, :] = ns.p_.vector.array.copy()
+            i_save += 1
